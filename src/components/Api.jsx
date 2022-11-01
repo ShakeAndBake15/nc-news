23b65fab761cefc441c42723ea852fb5af0e72d3
Alex Lyon <alexlyon@Alexs-MacBook-Air.local>
2022-11-01 16:51:32 +0000

    votes on article work in progress

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -19,3 +19,9 @@ export const getArticleById = (article_id) => {
         return response.data;
     })
 }
+
+export const patchArticleById = (article_id) => {
+    return newsApi.get(`/articles/${article_id}`, article_id).then((response) => {
+        return response.data;
+    })
+}
